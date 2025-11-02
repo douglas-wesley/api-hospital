481489c7507eaa06b8a6b1d0bc883478cc898bec
douglas-wesley <douglaswesley0407@gmail.com>
2025-11-02 14:29:19 -0300

    fix: Rename SecurityConfig package to improve project structure

--- src/main/java/com/douglas/APIHosital/config/SecurityConfig.java
+++ src/main/java/com/douglas/APIHosital/config/SecurityConfig.java
@@ -1,7 +1,7 @@
-package com.douglas.APIHosital.security;
+package com.douglas.APIHosital.config;
 
 
-import jakarta.servlet.Filter;
+import com.douglas.APIHosital.security.SecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
